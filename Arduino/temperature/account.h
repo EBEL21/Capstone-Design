441e8189cbc36a2e2f8a709f372ee22810f6438c
Taeho Kang <30804387+EBEL21@users.noreply.github.com>
2019-03-30 20:02:08 +0900

    Add files via upload

---
+++ Arduino/temperature/account.h
@@ -0,0 +1,5 @@
+String apiKey = "FU7IZ7HQIUUKKHP7";     //  Enter your Write API key from ThingSpeak
+const char* ssid = "KT_WLAN_79F6";     // Enter your WiFi Network's SSID
+const char* pwd = "0000006f62"; // Enter your WiFi Network's Password
+const char* server = "api.thingspeak.com";
+const char* ubuntuHost = "18.223.155.255";
--- Arduino/temperature/temperature.ino
+++ Arduino/temperature/temperature.ino
@@ -3,14 +3,10 @@
 #include <DallasTemperature.h>
 #include<time.h>
 #include<ArduinoJson.h>
+#include "account.h"
 
 #define ONE_WIRE_BUS 2
 
-String apiKey = "FU7IZ7HQIUUKKHP7";     //  Enter your Write API key from ThingSpeak
-const char* ssid =  "KT_WLAN_79F6";     // Enter your WiFi Network's SSID
-const char* pwd =  "0000006f62"; // Enter your WiFi Network's Password
-const char* server = "api.thingspeak.com";
-const char* ubuntuHost = "18.223.155.255";
 float temp;
 
 OneWire oneWire(ONE_WIRE_BUS);
